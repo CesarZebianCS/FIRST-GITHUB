617b8cff761bd60a0ac985d2a626a0ed5b9bae55
Cesar Zebian <zebianc4954@student.laccd.edu>
2025-09-07 12:10:37 -0700

    Add files via upload

---
+++ lab0.cpp
@@ -0,0 +1,38 @@
+// Cesar Zebian
+// Lab #0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+        cout << "\nMy name is "
+        "Cesar Zebian" << endl;
+
+    cout << "\nEnter the second number ";
+    cin >> num2;
+
+    cout << "\nEnter the first number  ";
+    cin >> num1;
+
+    sum = num1 / num2;
+    cout << "\nThe sum of " << num1 << " and " << sum << " is " << num2 << endl;
+
+    return 0;
+}
+
+/*
+  My name is Cesar Zebian
+
+Enter the second number 23
+
+Enter the first number  56
+
+The sum of 56 and 2 is 23
+
+C:\Users\cesar\source\repos\CesarZebianCS136\x64\Debug\CesarZebianCS136.exe (process 6088) exited with code 0.
+Press any key to close this window . . .
+
+*/
